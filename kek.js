51ba7015f9b5eff7eacb718eb9963287e27ec296
sevahasla <sevastesl@gmail.com>
2016-11-30 22:00:41 +0400

    added massiv

--- kek.js
+++ kek.js
@@ -1,5 +1,6 @@
 var name=prompt("Как вас зовут?");
-if(name=="Гена"){
+var genas=["гена","Гена","Геннадий","геннадий"];
+if(genas.includes(name)){
 	alert("Пшёл вон Гена!!!");
 	window.location="http://risovach.ru/upload/2014/10/mem/ashotik-car_63590666_orig_.jpeg"
 }
