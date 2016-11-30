c4135ccef5549b3430a357c9af08acc9f52963fd
sevahasla <sevastesl@gmail.com>
2016-11-30 21:42:55 +0400

    added location

--- kek.js
+++ kek.js
@@ -1,4 +1,5 @@
 var name=prompt("Как вас зовут?");
-if(name="Гена"){
+if(name=="Гена"){
 	alert("Пшёл вон Гена!!!");
+	window.location="http://risovach.ru/upload/2014/10/mem/ashotik-car_63590666_orig_.jpeg"
 }
