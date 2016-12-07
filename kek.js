ae07a5a2dff5a3a351de3199d3ad29635742b334
sevahasla <sevastesl@gmail.com>
2016-12-07 23:01:04 +0400

    added background and norm style

--- kek.js
+++ kek.js
@@ -1,6 +1,7 @@
-var name=prompt("Как вас зовут?");
+/*var name=prompt("Как вас зовут?");
 var genas=["гена","Гена","Геннадий","геннадий"];
 if(genas.includes(name)){
 	alert("Пшёл вон Гена!!!");
 	window.location="http://risovach.ru/upload/2014/10/mem/ashotik-car_63590666_orig_.jpeg"
 }
+*/
