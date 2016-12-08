eb10a4b3029e7dd786cb3d2fa330d2f7a5ccea50
sevahasla <sevastesl@gmail.com>
2016-12-08 15:54:45 +0400

    added API and play

--- kek.js
+++ kek.js
@@ -4,4 +4,15 @@ if(genas.includes(name)){
 	alert("Пшёл вон Гена!!!");
 	window.location="http://risovach.ru/upload/2014/10/mem/ashotik-car_63590666_orig_.jpeg"
 }
-*/
+
+var obj = {
+	kek: 228,
+	"lol": [1,2,3],
+	"123": {
+		"a": 123,
+		"b": function(){ console.log("kekkkkkk") }
+	}
+};
+
+console.log(obj["123"["a"]]);
+obj.123*/
