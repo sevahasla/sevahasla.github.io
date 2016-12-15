f635c0867eca5b11e22d343c4bbb03abe1b74ab9
sevahasla <sevastesl@gmail.com>
2016-12-15 13:39:25 +0400

    xz

--- server.js
+++ server.js
@@ -2,9 +2,9 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('ololo');
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!');
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
 });
