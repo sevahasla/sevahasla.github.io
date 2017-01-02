5df3f0fc38a18fa8899f9c1796604f8caee3e6f8
sevahasla <sevastesl@gmail.com>
2017-01-02 16:48:44 +0400

    added localStotage

--- kek2.js
+++ kek2.js
@@ -5,9 +5,10 @@ window.kek = function() {
     });
 }
 
-var randomRight = Math.floor(Math.random()*25);
-var randomLeft = Math.floor(Math.random()*25);
-var a=/*'qwertyuiopasdfghjklzxcvbnm'.split('')*/['q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m'];
+var randomRight = Math.floor(Math.random()*40);
+var randomLeft = Math.floor(Math.random()*40);
+var a=/*'qwertyuiopasdfghjklzxcvbnm'.split('')*/['q','w','e','r','t','y',
+'u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n','m','minecraft'];
 Array.prototype.getRandomElement=function(){
     var random=Math.floor(Math.random()*this.length);
     return this[random];
@@ -28,17 +29,18 @@ function search() {
         part:['snippet','statistics'],
         order: 'viewCount',
         type: 'video',
-        maxResults:'25'
+        maxResults:'40'
     }).execute(processRequest1);
 }
 function processRequest1(response) {
+    console.log(response);
     var videoIdLeft = response.items[randomLeft].id.videoId;
     var videoIdRight= response.items[randomRight].id.videoId;
     var iframeRight = createVideoIframer(videoIdRight);
     var iframeLeft = createVideoIframel(videoIdLeft);
 
-    $("div.videosRight").append(iframeRight);
-    $("div.videosLeft").append(iframeLeft);
+    $("div.videosRight")./*append*/html(iframeRight);
+    $("div.videosLeft")./*append*/html(iframeLeft);
 
     // Getting vidoes stats
     var ids = response.items.map(function(item, index, array) {
@@ -56,8 +58,8 @@ function processRequest2(response2) {
     console.info(response2);
     window.viewRight=response2.items[randomRight].statistics.viewCount;
     window.viewLeft=response2.items[randomLeft].statistics.viewCount;   
-    console.log('LEFT:',viewLeft);
-    console.log('RIGHT:',viewRight);
+    console.log('LEFT:   ',viewLeft);
+    console.log('RIGHT:  ',viewRight);
 }
 
 function createVideoIframer(videoIdRight) {
@@ -67,27 +69,37 @@ function createVideoIframer(videoIdRight) {
 function createVideoIframel(videoIdLeft) {
     return '<iframe width="420" height="315" src="https://www.youtube.com/embed/' + videoIdLeft + '"></iframe>';
 }
- var  score = 0;
+var score=parseInt(localStorage.getItem('score'));
+if (isNaN(score)){
+    score=0;
+}
+$('span.scoreNumber').html(score);
+function win() {
+    score++;
+    localStorage.setItem('score',score);
+    $('span.scoreNumber').html(score);
+}
+function lose() {
+    score = 0;
+    localStorage.setItem('score',score);
+    $('span.scoreNumber').html(score);
+}
 function guesserRight(){
-   // console.log('RIGHT:',viewRight);
     if(viewRight >= viewLeft){
-        score++;
+        win();
     }
     else{
-        score=0;
+        lose();
     }
-    $('span.scoreNumber').html(score);
     search();
 }
 function guesserLeft(){
-   // console.log('LEFT:',viewLeft);
     if(viewLeft >= viewRight){
-        score++;
+        win();
     }
     else{
-        score=0;
+        lose();
     }
-    $('span.scoreNumber').html(score);
     search();
 }
 $('.rightButton').on('click', guesserRight);
